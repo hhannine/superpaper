1556e6acc67b4fe76e344508eb7907839c291800
Henri Hänninen <henri.j.hanninen@gmail.com>
2020-04-10 13:13:07 +0300

    Update run wrapper

--- pyinstaller_wrapper.py
+++ pyinstaller_wrapper.py
@@ -3,7 +3,7 @@ Simple wrapper that makes the module structure of Superpaper
 compatible with PyInstaller builds.
 """
 
-from superpaper.superpaper import main
+from superpaper.__main__ import main
 
 if __name__ == '__main__':
     main()
