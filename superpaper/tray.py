4c0f262860d0ca22c784a46b4ec43cf6334a4887
Henri Hänninen <47873564+hhannine@users.noreply.github.com>
2020-04-28 22:12:53 +0300

    Set locale explicitly to prevent conflict with system

--- superpaper/tray.py
+++ superpaper/tray.py
@@ -462,6 +462,7 @@ class App(wx.App):
     def OnInit(self):
         """Starts tray icon loop."""
         frame = wx.Frame(None)
+        self.locale = wx.Locale(wx.LANGUAGE_ENGLISH)
         self.SetTopWindow(frame)
         TaskBarIcon(frame)
         return True
