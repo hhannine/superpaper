d5663a6dc7ccbc4cca3501f3a749e8d345084a95
Henri Hänninen <henri.j.hanninen@gmail.com>
2020-10-17 13:22:16 +0300

    Change default locale to the system default

--- superpaper/tray.py
+++ superpaper/tray.py
@@ -466,7 +466,7 @@ class App(wx.App):
     def OnInit(self):
         """Starts tray icon loop."""
         frame = wx.Frame(None)
-        self.locale = wx.Locale(wx.LANGUAGE_ENGLISH_US)
+        self.locale = wx.Locale(wx.LANGUAGE_DEFAULT)
         self.SetTopWindow(frame)
         TaskBarIcon(frame)
         return True
